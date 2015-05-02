718db46ad3daa3a4d5ca785636fb2f4beae9932a
Chris Orem <chrisorem@gmail.com>
2015-05-02 11:39:13 -0400

    working on button reading

--- neato_node/nodes/neato.py
+++ neato_node/nodes/neato.py
@@ -149,8 +149,7 @@ class NeatoNode:
             button_enum = ("Soft_Button", "Up_Button", "Start_Button", "Back_Button", "Down_Button")
 
             for idx, b in enumerate({btn_soft, btn_scr_up, btn_start, btn_back, btn_scr_down}):
-                print b
-                if b == 'True':
+                if b == 1:
                     button.value = b
                     button.name = button_enum[idx]
                     self.buttonPub.publish(button)
